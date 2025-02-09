450e412de46138acdbea2a803b4a545a734861f5
leeseungji <94357128+leeseungji@users.noreply.github.com>
2025-02-09 15:07:07 +0900

    [Bronze I] Title: 2007년, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,8 +1,8 @@
-m, d = map(int, input().split())
-
 month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+m, d = map(int, input().split())
 
 total = sum(month[:m-1]) + d
 
-print(week[(total-1) % 7])
+print(week[total % 7])
