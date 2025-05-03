2619492df6a96e7fef97d49c10306d11bf496071
leeseungji <94357128+leeseungji@users.noreply.github.com>
2025-05-03 16:54:14 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.py
@@ -0,0 +1,13 @@
+def solution(num):
+    answer = ''
+    
+    if num % 2 == 0:
+        answer = 'Even'
+    
+    elif num == 0:
+        answer = 'Even'
+    
+    else:
+        answer = 'Odd'
+    
+    return answer
