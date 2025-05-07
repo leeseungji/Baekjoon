5983858a963e56e16b5e17dced9236bce41c7f4c
leeseungji <94357128+leeseungji@users.noreply.github.com>
2025-05-07 16:56:13 +0900

    [level 1] Title: 모의고사, Time: 5.88 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,30 @@
+def solution(answers):
+    answer = []
+    
+    patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    
+    scores = [0] * 3
+    
+    for i in range (len(answers)):
+        if answers[i] == patterns[0][i % len(patterns[0])]:
+            scores[0] += 1
+        
+        if answers[i] == patterns[1][i % len(patterns[1])]:
+            scores[1] += 1
+        
+        if answers[i] == patterns[2][i % len(patterns[2])]:
+            scores[2] += 1
+        
+    max_scores = max(scores)
+    
+    for i in range(3):
+        if scores[i] == max_scores:
+            answer.append(i+1)
+            
+    
+    
+    return answer
